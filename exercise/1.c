0a23e87d38f2c835bab5f7754ea3ef5c3c5bed1d
douzi <2083772270@qq.com>
2024-11-06 00:06:47 +0800

    exam1

--- exercise/1.c
+++ exercise/1.c
@@ -10,15 +10,15 @@
 
 void my_power(double x, double y, double* mypow)
 {
-	*mypow = pow(x, y);//调用pow函数计算x^y的值并存入mypow所指向的存储单元
+	*mypow = pow(x,y);//调用pow函数计算x^y的值并存入mypow所指向的存储单元
 }
 
 int main()
 {
 	double a, b, pow, * ppow = &pow;//指针ppow指向变量pow
 	printf("Please input two floating point numbers separated by spaces:\n");
-	scanf_s("%lf%lf", &a, &b);
-	my_power(a, b, ppow);
-	printf("%.2lf to %.2lf is %.2lf.\n", a, b, pow);
+	scanf("%lf%lf",&a,&b);
+	my_power(a,b,ppow);
+	printf("%.2lf to %.2lf is %.2lf.\n",a,b,pow);
 	return 0;
 }
